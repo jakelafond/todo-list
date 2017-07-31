1f1f9e1648bb2d26a2059cde6150c84bb245da73
Jake LaFond <jakelafond@gmail.com>
2017-07-31 16:20:52 -0400

    now really done with sessions

--- app.js
+++ app.js
@@ -2,32 +2,55 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
 const bodyParser = require('body-parser');
+const expressSession = require('express-session');
 var todos = [];
 var completed = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  expressSession({
+    secret: 'the one and only',
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/', (req, res) => {
-  res.render('index', { todos: todos, completed: completed });
+  const todosOpen = req.session.todosOpen || [];
+  const todosCompleted = req.session.todosCompleted || [];
+
+  res.render('index', { todosOpen, todosCompleted });
 });
 
 app.post('/add', (req, res) => {
-  todos.push(req.body.todo);
+  const todosOpen = req.session.todosOpen || [];
+  const todosCompleted = req.session.todosCompleted || [];
+
+  todosOpen.push(req.body.todo);
+
+  req.session.todosOpen = todosOpen;
+
   res.redirect('/');
 });
 
 app.post('/completed', (req, res) => {
-  var i = todos.indexOf(req.body.marked);
+  const todosOpen = req.session.todosOpen || [];
+  const todosCompleted = req.session.todosCompleted || [];
+
+  var i = todosOpen.indexOf(req.body.marked);
   if (i != -1) {
-    todos.splice(i, 1);
-    console.log(todos);
-    completed.push(req.body.marked);
+    todosOpen.splice(i, 1);
+    console.log(todosOpen);
+    todosCompleted.push(req.body.marked);
   }
+
+  req.session.todosCompleted = todosCompleted;
+
   res.redirect('/');
 });
 
